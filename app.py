de2cdeb6cd322a465919c0c1b1e573d5a54bf0ef
jjordan19 <jjordan1@vt.edu>
2021-12-09 05:46:50 +0000

    add quotes, list quotes, html template

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template
+import flask
+from markupsafe import escape
+import pymongo
+import json
+from bson import json_util
+
+
+# Creates MongoDB connection between application and database.
+#db = pymongo.MongoClient("mongodb+srv://admin-python-test-app:QSzVf6aJw5XM6t1v@cluster0.vzhrl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+app = Flask(__name__)
+
+# Create mindlycleaser database
+db = pymongo.MongoClient("mongodb://127.0.0.1:27017/?compressors=disabled&gssapiServiceName=mongodb") # Connects to local MongoDB
+
+database = db["mindlycleanser"]
+collection = database["quotes"]
+
+@app.route("/")
+def home_page():
+    return render_template('index.html', title="MindlyCleanser")
+    #return render_template('index2.html')
+
+# Create a quote using the REST API
+@app.route("/add/<int:quote_id>/<author>/<quote>/")
+def add_quote(quote_id, author, quote):
+    try:
+        quote = quote.replace("-", " ")
+        collection.insert_one({"_id": f"{quote_id}", "author": f"{author}", "quote": f"{quote}"})
+        return flask.jsonify(message="success")
+    except Exception as DuplicateKeyError:
+        existing = collection.find({"_id": f"{quote_id}"})
+        if existing:
+            total = list(collection.find({}))
+            len_total = len(total)
+            quote_id = len_total + 1
+            collection.insert_one({"_id": f"{quote_id}", "author": f"{author}", "quote": f"{quote}"})    
+        return flask.jsonify(message=f"New ID: {quote_id}")
+ 
+    return flask.jsonify(message="success")
+
+@app.route("/quotes")
+def get_quote():
+    all_quotes = list(collection.find({}))
+    return json.dumps(all_quotes)
+
+
+if __name__ == "__main__":
+    app.run(host="10.0.0.14", port=5000, debug=True)
